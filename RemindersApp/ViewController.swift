5e176204b777af6cfb2f134a3cc613fb247993d4
Silas Hayri <silashayri@hotmail.com>
2023-04-24 22:17:06 +0300

    Initial Commit

---
+++ RemindersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by Silas Hayri on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
